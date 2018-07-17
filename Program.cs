f54dff9507535c314a92a45f1d53a5ebbd22aaf5
BijayaAcharya <bijaya2005@hotmail.com>
2018-07-17 19:59:21 -0400

    comment added

--- Program.cs
+++ Program.cs
@@ -11,64 +11,64 @@ namespace Lab2FindTheAreaAndPerimeter
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome to Grand Circus' Room Detail Generator!");
-
-            // asking user input 1
-            Console.WriteLine("enter the length");
-
-            //readiing from the console and parsing to double
-            double length = double.Parse(Console.ReadLine());
-
-            // asking user input II
-            Console.WriteLine("enter the width");
-
-            // reading from the console and parsing to double
-            double width = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("enter the height");
-            double height = double.Parse(Console.ReadLine());
-
-            // calculating area
-            double area = length * width;
-            // calculating perimeter
-            double peri = (2 * (length + width));
-            // calculating volume
-            double volume = (length * width * height);
-
-            //displaying the output to the console
-
-            Console.WriteLine("Area of Room is = {0}, Perimeter is ={1}, and volume is {2}", area, peri, volume);
-
-            //Console.WriteLine("Area of Room is = {0} and Perimeter is ={1}", area, peri);
-            Console.ReadLine();
-
-            // using while loop
             while (true)
             {
+               
+                // asking user input 1
+                Console.WriteLine("enter the length");
+
+                //readiing from the console and parsing to double
+                double length = double.Parse(Console.ReadLine());
+
+                // asking user input II
+                Console.WriteLine("enter the width");
+
+                // reading from the console and parsing to double
+                double width = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("enter the height");
+                double height = double.Parse(Console.ReadLine());
+
+                // calculating area
+                double area = length * width;
+                // calculating perimeter
+                double peri = (2 * (length + width));
+                // calculating volume
+                double volume = (length * width * height);
+
+                //displaying the output to the console
+
+                Console.WriteLine("Area of Room is = {0}, Perimeter is ={1}, and volume is {2}", area, peri, volume);
+
+                //Console.WriteLine("Area of Room is = {0} and Perimeter is ={1}", area, peri);
+                Console.WriteLine("");
+
+                
                 Console.WriteLine(" Do you want to continue?, 'y/n'"); // asking user input
                 string answer = Console.ReadLine().ToUpper(); // reading from the console and converting user input into upper case
                 if (answer == "Y") // condition yes
                 {
-                } 
-                                       
+                }
+
                 else
                 {
                     break; // exit the program
                 }
-                Console.ReadLine();
+                
             }
         }
     }
 }
 
-            
-            
-
-            
-
-            
-           
 
 
-            
-       
-    
+
+
+
+
+
+
+
+
+
+
